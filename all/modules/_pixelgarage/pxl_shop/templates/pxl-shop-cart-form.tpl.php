07b0bdfba8b01dcbf0562656511f638bc860ffc9
Ralph Moser <ralph@ramosoft.ch>
2017-02-22 19:04:00 +0100

    - pxl-shop forms with wrapper

--- all/modules/_pixelgarage/pxl_shop/templates/pxl-shop-cart-form.tpl.php
+++ all/modules/_pixelgarage/pxl_shop/templates/pxl-shop-cart-form.tpl.php
@@ -7,17 +7,18 @@
  */
 ?>
 
-<div id="<?php print $block_id; ?>" class="block block-shopping-cart-form">
-  <!-- The form section -->
-  <div class="shopping-cart-table"><?php print $shopping_cart; ?></div>
+<div class="form-wrapper">
+  <div id="<?php print $block_id; ?>" class="block block-shopping-cart-form">
+    <!-- The form section -->
+    <div class="shopping-cart-table"><?php print $shopping_cart; ?></div>
 
-  <!-- The actions section -->
-  <div class="shopping-cart-actions">
-    <button type="button" class="cart-items-reset" data-item-id="all"><?php print $button_label_reset ; ?></button>
-    <?php print $stripe_button; ?>
+    <!-- The actions section -->
+    <div class="shopping-cart-actions">
+      <button type="button" class="cart-items-reset" data-item-id="all"><?php print $button_label_reset ; ?></button>
+      <?php print $stripe_button; ?>
+    </div>
+    <div class="label label-danger"></div>
   </div>
-  <div class="label label-danger"></div>
-
 </div>
 
 
